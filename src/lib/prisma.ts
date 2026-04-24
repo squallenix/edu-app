707aebaf254b557ede4c2c56f5d51c8bdbc3b1a4
squallenix <squallenix12@gmail.com>
2026-04-25 04:43:27 +0600

    .

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,17 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+const mariadbAdapter = new PrismaMariaDb(process.env.DATABASE_URL ?? "");
+
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter: mariadbAdapter,
     log: ["error"],
   });
 

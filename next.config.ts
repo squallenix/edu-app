58e9da80faf5b1f54c178f7ac9d73e6985067540
squallenix <squallenix12@gmail.com>
2026-04-25 03:09:36 +0600

    modified:   next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'export', // Enables static export
+  images: { unoptimized: true },
 };
 
 export default nextConfig;

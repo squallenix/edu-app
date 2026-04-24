0273fe24b3c7a89ff3bade3e1a92bdc24c84c739
squallenix <squallenix12@gmail.com>
2026-04-25 04:05:02 +0600

    modified:   src/app/api/(auth)/[...nextauth]/route.ts 	modified:   src/app/api/(auth)/signup/route.ts

--- src/app/api/(auth)/[...nextauth]/route.ts
+++ src/app/api/(auth)/[...nextauth]/route.ts
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
-
 import { prisma } from "@/lib/prisma";
+export const runtime = "nodejs";
 const handler = NextAuth({
     providers: [
         CredentialsProvider({
--- src/app/api/(auth)/signup/route.ts
+++ src/app/api/(auth)/signup/route.ts
@@ -1,5 +1,4 @@
 import { NextResponse } from "next/server";
-
 import bcrypt from "bcrypt";
 import { prisma } from "@/lib/prisma";
 
